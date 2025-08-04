bd861761640df3f5bc56a3a10066a038248d1ea5
ihd12 <hyedonglim@gmail.com>
2025-08-04 10:17:11 +0900

    project init

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article(title,content,created_at, updated_at, author) VALUES('제목 1','내용 1', NOW(),NOW(), 'hong');
+INSERT INTO article(title,content,created_at, updated_at, author) VALUES('제목 2','내용 2', NOW(),NOW(), 'test');
+INSERT INTO article(title,content,created_at, updated_at, author) VALUES('제목 3','내용 3', NOW(),NOW(), 'lee');
